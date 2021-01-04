68b1e07cc7cede6b001f1d6c8883cae940da17b6
TsaoLin <b06901045@ntu.edu.tw>
2021-01-04 21:54:37 +0800

    taiwan map api/ main view

--- src/containers/TourMap.js
+++ src/containers/TourMap.js
@@ -1,22 +1,75 @@
 import React from "react"
+import Map from "../components/Map"
 import "./TourMap.css"
+import 'antd/dist/antd.css'
 
-export default function TourMap(){
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+
+const { SubMenu } = Menu;
+const { Header, Content, Footer, Sider } = Layout;
+
+const TourMap = () => {
     return (
-            <React.Fragment>
-                <div class="header">
-                    <input/>
-                </div>
-                <div class="body">
-                    <nav class="sidebar">
-                        <div> Place </div>
-                        <div> Address </div>
-                        <img/>
-                        <div> Comment </div>
-                        <h4> Invitation </h4>
-                        Jose Antonio's also interest in this place
-                    </nav>
-                </div>
-            </React.Fragment>
-        )
+    <React.Fragment>
+        <Layout>
+            <Header className="header">
+                <div className="logo" />
+                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+                    <Menu.Item key="1">nav 1</Menu.Item>
+                    <Menu.Item key="2">nav 2</Menu.Item>
+                    <Menu.Item key="3">nav 3</Menu.Item>
+                </Menu>
+            </Header>
+            <Layout>
+            <Sider width={200} className="site-layout-background">
+                <Menu
+                mode="inline"
+                defaultSelectedKeys={['1']}
+                defaultOpenKeys={['sub1']}
+                style={{ height: '100%', borderRight: 0 }}
+                >
+                <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
+                    <Menu.Item key="1">option1</Menu.Item>
+                    <Menu.Item key="2">option2</Menu.Item>
+                    <Menu.Item key="3">option3</Menu.Item>
+                    <Menu.Item key="4">option4</Menu.Item>
+                </SubMenu>
+                <SubMenu key="sub2" icon={<LaptopOutlined />} title="subnav 2">
+                    <Menu.Item key="5">option5</Menu.Item>
+                    <Menu.Item key="6">option6</Menu.Item>
+                    <Menu.Item key="7">option7</Menu.Item>
+                    <Menu.Item key="8">option8</Menu.Item>
+                </SubMenu>
+                <SubMenu key="sub3" icon={<NotificationOutlined />} title="subnav 3">
+                    <Menu.Item key="9">option9</Menu.Item>
+                    <Menu.Item key="10">option10</Menu.Item>
+                    <Menu.Item key="11">option11</Menu.Item>
+                    <Menu.Item key="12">option12</Menu.Item>
+                </SubMenu>
+                </Menu>
+            </Sider>
+            <Layout style={{ padding: '0 24px 24px' }}>
+                <Breadcrumb style={{ margin: '16px 0' }}>
+                <Breadcrumb.Item>Home</Breadcrumb.Item>
+                <Breadcrumb.Item>List</Breadcrumb.Item>
+                <Breadcrumb.Item>App</Breadcrumb.Item>
+                </Breadcrumb>
+                <Content
+                className="site-layout-background"
+                style={{
+                    padding: 24,
+                    margin: 0,
+                    minHeight: 280,
+                }}
+                >
+                <Map></Map>
+                </Content>
+            </Layout>
+            </Layout>
+        </Layout>
+    </React.Fragment>
+    )
 }
+
+export default TourMap;
