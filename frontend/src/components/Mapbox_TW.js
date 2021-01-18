aa1611d94eb65d90e0d0fb53d43d3d86dc046324
smarthercules <b06901041@ntu.edu.tw>
2021-01-18 21:52:03 +0800

    setlnglat

--- frontend/src/components/Mapbox_TW.js
+++ frontend/src/components/Mapbox_TW.js
@@ -11,10 +11,16 @@ const MapBox = () => {
     lat: 25.05,
     zoom: 12
   })
+
+  const [lng, setLng] = useState(121.55);
+  const [lat, setLat] = useState(25.05);
+  const [zoom, setZoom] = useState(12);
+
   const [map, setMap] = useState(null);
   const mapContainer = useRef(null);
 
-  const [clicklngLat, setClicklngLat] = useState([]);
+  const [clicklnglat, setClicklnglat] = useState([0, 0]);
+  const [test, setTest] = useState(0);
 
   useEffect(() => {
     const initializeMap = ({ setMap, mapContainer }) => {
@@ -24,33 +30,42 @@ const MapBox = () => {
         center: [View.lng, View.lat],
         zoom: View.zoom
       });
-
-      map.on("load", () => {
-        setMap(map);
-        map.resize();
-      });
-
-      map.on('click', clickPoint);
-      function clickPoint(e){
-        setClicklngLat([e.lngLat.lng, e.lngLat.lat])
-        console.log(e.lngLat)
-        console.log(clicklngLat)
-        var popup = new mapboxgl.Popup()
-        .setHTML('<h3>A point</h3>');
-        
-        var marker = new mapboxgl.Marker()
-        .setLngLat([e.lngLat.lng, e.lngLat.lat])
-        .setPopup(popup)
-        .addTo(map);  
-      };
     }
     if (!map) initializeMap({ setMap, mapContainer });
   }, [map]);
+
+  if(map){
+    map.on("load", () => {
+      setMap(map);
+      map.resize();
+    });
+
+    map.on("move", () => {
+      setLng(map.getCenter().lng.toFixed(4));
+      setLat(map.getCenter().lat.toFixed(4));
+      setZoom(map.getZoom().toFixed(2));
+    });  
+
+    const clickPoint = (e) => {
+      setClicklnglat([e.lngLat.lng, e.lngLat.lat])
+      console.log([e.lngLat.lng, e.lngLat.lat])
+      var popup = new mapboxgl.Popup()
+      .setHTML('<h3>A point</h3>');
+      
+      var marker = new mapboxgl.Marker()
+      .setLngLat([e.lngLat.lng, e.lngLat.lat])
+      .setPopup(popup)
+      .addTo(map);  
+    };
+
+    map.on('click', clickPoint);
+  }
   
   return (
     <div>
       <div className='sidebarStyle'>
-        <div>Longitude: {View.lng} | Latitude: {View.lat} | Zoom: {View.zoom}</div>
+        {/* <div>Longitude: {View.lng} | Latitude: {View.lat} | Zoom: {View.zoom}</div> */}
+        <div>Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}</div>
       </div>
       <div ref={el => (mapContainer.current = el)} className='mapContainer'/>
     </div>
