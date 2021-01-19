e245192841f1687e5909a567d2ae8c3f113d586d
smarthercules <b06901041@ntu.edu.tw>
2021-01-19 23:18:27 +0800

    account button & planList-v0

--- frontend/src/containers/PlanList.js
+++ frontend/src/containers/PlanList.js
@@ -1,44 +1,16 @@
 import React, { useState } from "react"
-import MapBox from "../components/Mapbox_TW"
-import AddPlace from "../components/addPlace"
-import SearchInfo from "../components/SearchInfo"
-import Background from '../img/background.jpg';
 import { Form, Checkbox, Input, Layout, Menu, Breadcrumb, Button } from 'antd';
 import { LockOutlined, UserOutlined, PictureOutlined, EnvironmentOutlined} from '@ant-design/icons';
-import "./TourMap.css"
-import "./TourMap.css"
 import 'antd/dist/antd.css'
 
-// import {
-//     MARKER_QUERY,
-//     ADD_MARKER_MUTATION,
-//     MARKERS_SUBSCRIPTION
-//   } from '../graphql'
 
-const { SubMenu } = Menu;
 const { Header, Content, Footer, Sider } = Layout;
 
-const TourMap = () => {
-    const [username, setUsername] = useState("")
-    const [usernamedecided, setUsernamedecided] = useState(false)
-    const onFinish = (values) => {
-        console.log('Received values of form: ', values);
-        setUsername(values.username)
-        setUsernamedecided(true)
-      };
-    
-    //new add
-    //accout -> plan list
-    const [showUsermMenu, setShowUsermMenu] = useState(false)
-    
-    // const { subscribeToMore, ...result } = useQuery(
-    //     MARKER_QUERY,
-    //     { variables: { username: username } }
-    //   )
-
+const PlanList = () => {  
+    plans=[]  
     return (
     <React.Fragment>
-        {usernamedecided? ( // 決定是否有輸入 username
+        {plans? ( // 決定是否已有任何的plan
         <React.Fragment>
             <Layout>
             <Header className="header" style={{margin: "0px"}}>
@@ -60,34 +32,6 @@ const TourMap = () => {
                 </Menu>
             </Header>
             <Layout>
-            <Sider height={500} width={200} className="site-layout-background">
-                <AddPlace></AddPlace>
-                {/* <Menu
-                mode="inline"
-                defaultSelectedKeys={['1']}
-                defaultOpenKeys={['sub1']}
-                style={{ height: '100%', borderRight: 0}}
-                >
-                <SubMenu key="sub1" icon={<EnvironmentOutlined />} title="地點">
-                    <Menu.Item key="1">option1</Menu.Item>
-                    <Menu.Item key="2">option2</Menu.Item>
-                    <Menu.Item key="3">option3</Menu.Item>
-                    <Menu.Item key="4">option4</Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub2" icon={<PictureOutlined />} title="相片">
-                    <Menu.Item key="5">option5</Menu.Item>
-                    <Menu.Item key="6">option6</Menu.Item>
-                    <Menu.Item key="7">option7</Menu.Item>
-                    <Menu.Item key="8">option8</Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub3" icon={<UserOutlined />} title="揪團成員">
-                    <Menu.Item key="9">option9</Menu.Item>
-                    <Menu.Item key="10">option10</Menu.Item>
-                    <Menu.Item key="11">option11</Menu.Item>
-                    <Menu.Item key="12">option12</Menu.Item>
-                </SubMenu>
-                </Menu> */}
-            </Sider>
             <Layout style={{ padding: '0 24px 24px' }}>
                 <Breadcrumb style={{ margin: '16px 0' }}>
                 <Breadcrumb.Item>Home</Breadcrumb.Item>
@@ -184,4 +128,4 @@ const TourMap = () => {
     )
 }
 
-export default TourMap;
+export default PlanList;
