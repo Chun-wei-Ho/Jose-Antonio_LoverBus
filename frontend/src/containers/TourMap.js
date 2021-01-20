814de6ef048affa7f387b8636449a98e7a138f4a
TsaoLin <b06901045@ntu.edu.tw>
2021-01-21 01:17:12 +0800

    tourmap header

--- frontend/src/containers/TourMap.js
+++ frontend/src/containers/TourMap.js
@@ -56,19 +56,9 @@ const TourMap = (props) => {
     return (
         <React.Fragment>
             <Layout>
-            <Header className="header">
+            <Header className="header" style={{color: "white", fontSize: "20px"}}>Tour Map
                 <div className="logo" />
-                {/* <Input></Input> */}
                 <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-                    <Menu.Item key="1" style={{position: "relative", bottom: "100px"}}>
-                        <SearchInfo></SearchInfo>
-                    </Menu.Item>
-                    <Menu.Item key="2" style={{left: '0px'}}>
-                        nav 1
-                    </Menu.Item>
-                    <Menu.Item key="3" style={{left: '0px'}}>
-                        nav 2
-                    </Menu.Item>
                     <Menu.Item key="4" style={{position:'absolute', width: '150px',textAlign: 'center', right: '0%'}} onClick={() => {setShowUsermMenu(!showUsermMenu)}}>
                         {username}
                     </Menu.Item>
@@ -77,31 +67,6 @@ const TourMap = (props) => {
             <Layout>
             <Sider height={500} width={200} className="site-layout-background">
                 <AddPlace username={username} currentMarker={currentMarker}></AddPlace>
-                {/* <Menu
-                mode="inline"
-                defaultSelectedKeys={['1']}
-                defaultOpenKeys={['sub1']}
-                style={{ height: '100%', borderRight: 0}}
-                >
-                <SubMenu key="sub1" icon={<EnvironmentOutlined />} title="地點">
-                    <Menu.Item key="1">option1</Menu.Item>
-                    <Menu.Item key="2">option2</Menu.Item>
-                    <Menu.Item key="3">option3</Menu.Item>
-                    <Menu.Item key="4">option4</Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub2" icon={<PictureOutlined />} title="相片">
-                    <Menu.Item key="5">option5</Menu.Item>
-                    <Menu.Item key="6">option6</Menu.Item>
-                    <Menu.Item key="7">option7</Menu.Item>
-                    <Menu.Item key="8">option8</Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub3" icon={<UserOutlined />} title="揪團成員">
-                    <Menu.Item key="9">option9</Menu.Item>
-                    <Menu.Item key="10">option10</Menu.Item>
-                    <Menu.Item key="11">option11</Menu.Item>
-                    <Menu.Item key="12">option12</Menu.Item>
-                </SubMenu>
-                </Menu> */}
             </Sider>
             <Layout style={{ padding: '0 24px 24px' }}>
                 <Breadcrumb style={{ margin: '16px 0' }}>
@@ -134,11 +99,6 @@ const TourMap = (props) => {
                 </Content>
             </Layout>
             </Layout>
-            <Footer className="Footer">
-                <Button className="FooterButton">Finish</Button>
-                <Button className="FooterButton">Wish</Button>
-                <Button className="FooterButton">All</Button>
-            </Footer>
             </Layout>
         </React.Fragment>
     )
