432bf4272cb72be8a84082fe54f26a3a22c8f7cd
B06901141 <b06901141@ntu.edu.tw>
2021-01-21 19:51:43 +0800

    heroku-client

--- backend/src/models/index.js
+++
@@ -1,8 +0,0 @@
-const Marker = require('./Marker')
-const Plan = require('./Plan')
-const User = require('./User')
-const Spot = require('./Spot')
-
-const models = {Marker, Plan, User, Spot}
-
-module.exports = models
--- backend/src/resolvers/index.js
+++
@@ -1,7 +0,0 @@
-const Query = require('./Query')
-const Mutation = require('./Mutation')
-const Subscription = require('./Subscription')
-
-const resolvers = {Query, Mutation, Subscription}
-
-module.exports = resolvers 
