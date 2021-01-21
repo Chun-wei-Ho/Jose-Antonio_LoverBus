0cc3b6fbb046d5cebb8d2b2bb978cfcf0ad20448
smarthercules <b06901041@ntu.edu.tw>
2021-01-21 15:59:39 +0800

    home/list/app

--- frontend/src/containers/TourMap.js
+++ frontend/src/containers/TourMap.js
@@ -82,8 +82,6 @@ const TourMap = (props) => {
             <Layout style={{ padding: '0 24px 24px' }}>
                 <Breadcrumb style={{ margin: '16px 0' }}>
                 <Breadcrumb.Item>Home</Breadcrumb.Item>
-                <Breadcrumb.Item>List</Breadcrumb.Item>
-                <Breadcrumb.Item>App</Breadcrumb.Item>
                 </Breadcrumb>
                 <Content
                 className="site-layout-background"
